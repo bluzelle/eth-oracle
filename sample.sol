a77832f4f7ff1172e5e1ae57d9765e696fa2909f
isabel <isabel@izabyl.com>
2018-04-27 21:24:53 +0000

    sample client updated

--- sample.sol
+++ sample.sol
@@ -5,30 +5,58 @@ import "https://github.com/bluzelle/eth-oracle/bluzelle.sol";
 /* This is a minimal sample client that watches the value of a single key in
  * Bluzelle */
 contract SampleClient is BluzelleClient {
-    
-    string private key;
+
+    string public key;
     string public value;
     
+    bool public keyExists = false;
+
     address private owner = msg.sender;
-    
+
     constructor(string _key, string _uuid) public {
+        require(bytes(_key).length > 0);
+        require(bytes(_uuid).length > 0);
         key = _key;
         owner = msg.sender;
         setUUID(_uuid);
+        create(_key, "initial value");
     }
-    
+
     /* Read the value from Bluzelle (this requires a small fee to pay Oraclize) */
     function update() public payable {
         require(msg.sender == owner);
         read(key);
     }
     
-    function readCallback(string /*unused*/, string v) internal {
+    /* Set the value */
+    function set(string _value) public payable {
+        require(msg.sender == owner);
+        update(key, _value);
+    }
+
+    /* callback invoked by bluzelle upon read */
+    function readResult(string /*unused*/, string v) internal {
         value = v;
     }
     
-    function refund() public {
+    /* callback invoked by bluzelle upon create */
+    function createResponse(string /*unused*/, bool success) internal {
+        if(success){
+            keyExists = true;
+        }
+    }
+    
+    /* callback invoked by bluzelle upon delete */
+    function removeResponse(string /*unusued*/, bool success) internal {
+        if(success){
+            keyExists = false;
+        }
+    }
+
+    function refund() public payable {
         require(msg.sender == owner);
+        remove(key);
         owner.transfer(address(this).balance);
     }
 }
+
